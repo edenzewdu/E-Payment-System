f03731b128bc1e04ab71de86cd46fdc9a3a3293c
eden zewdu <edenzewdu434@gmail.com>
2023-08-02 14:01:53 +0300

    controller and route creation

--- back-end/models/junctionTable.js
+++
@@ -1,7 +0,0 @@
-// Define User-Agents junction table
-const UserAgent = sequelize.define('userAgent', {});
-// Define Agents-ServiceProvider junction table
-const AgentServiceProvider = sequelize.define('agentServiceProvider', {});
-// Define User-ServiceProvider junction table
-const UserServiceProvider = sequelize.define('userServiceProvider', {});
-
