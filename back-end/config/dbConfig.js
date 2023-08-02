9cb6128ab13bbd9519e4116626637227db7d594a
eden zewdu <edenzewdu434@gmail.com>
2023-08-02 07:53:33 +0300

    creating bill table

--- back-end/config/dbConfig.js
+++ back-end/config/dbConfig.js
@@ -1,15 +1,19 @@
-
-module.exports = {
+var mysql = require('mysql2')
+const connection = mysql.createConnection({
     HOST: 'localhost',
     USER: 'root',
-    PASSWORD: "12345678",
+    PASSWORD: '12345678',
     DB: 'epayment',
+    port: 3306,
+    multipleStatements: true,
     dialect: 'mysql',
 
     pool: {
         max: 5,
         min: 0,
-        acquire: 10000,
+        acquire: 30000,
         idle:10000
     }
-}
+})
+
+module.exports = connection;
