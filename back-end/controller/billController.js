79fddf257ded1030a97e25128938f866df9adc16
eden zewdu <edenzewdu434@gmail.com>
2023-08-03 15:32:19 +0300

    adding async await with error handler

--- back-end/controller/billController.js
+++ back-end/controller/billController.js
@@ -1,13 +1,14 @@
+const asyncHandler = require('express-async-handler');
 const db = require('../models');
 
 const Bill = db.Bill;
 
 // Create and save a new bill
-exports.create = (req, res) => {
+exports.create = asyncHandler(async (req, res) => {
   // Validate request
   if (!req.body.amount || !req.body.description) {
     res.status(400).send({
-      message: 'Amount and description cannot be empty'
+      message: 'Amount and description cannot be empty',
     });
     return;
   }
@@ -15,103 +16,69 @@ exports.create = (req, res) => {
   // Create a bill object
   const bill = {
     amount: req.body.amount,
-    description: req.body.description
+    description: req.body.description,
   };
 
   // Save bill in the database
-  Bill.create(bill)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || 'Some error occurred while creating the bill.'
-      });
-    });
-};
+  const data = await Bill.create(bill);
+  res.send(data);
+});
 
 // Retrieve all bills from the database
-exports.findAll = (req, res) => {
-  Bill.findAll()
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message:
-          err.message || 'Some error occurred while retrieving bills.'
-      });
-    });
-};
+exports.findAll = asyncHandler(async (req, res) => {
+  const data = await Bill.findAll();
+  res.send(data);
+});
 
 // Find a single bill by id
-exports.findOne = (req, res) => {
+exports.findOne = asyncHandler(async (req, res) => {
   const id = req.params.id;
 
-  Bill.findByPk(id)
-    .then(data => {
-      if (!data) {
-        res.status(404).send({
-          message: `Bill with id=${id} not found`
-        });
-      } else {
-        res.send(data);
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: `Error retrieving bill with id=${id}`
-      });
+  const data = await Bill.findByPk(id);
+  if (!data) {
+    res.status(404).send({
+      message: 
+      (`Bill with id=${id} not found`),
     });
-};
+  } else {
+    res.send(data);
+  }
+});
 
 // Update a bill by id
-exports.update = (req, res) => {
+exports.update = asyncHandler(async (req, res) => {
   const id = req.params.id;
 
-  Bill.update(req.body, {
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: 'Bill was updated successfully.'
-        });
-      } else {
-        res.send({
-          message: `Cannot update bill with id=${id}. Bill not found or req.body is empty!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: `Error updating bill with id=${id}`
-      });
+  const [num] = await Bill.update(req.body, {
+    where: { id: id },
+  });
+
+  if (num === 1) {
+    res.send({
+      message: 'Bill was updated successfully.',
     });
-};
+  } else {
+    res.send({
+      message: (`Cannot update bill with id=${id}. Bill not found or req.body is empty!`),
+    });
+  }
+});
 
 // Delete a bill by id
-exports.delete = (req, res) => {
+exports.delete = asyncHandler(async (req, res) => {
   const id = req.params.id;
 
-  Bill.destroy({
-    where: { id: id }
-  })
-    .then(num => {
-      if (num == 1) {
-        res.send({
-          message: 'Bill was deleted successfully!'
-        });
-      } else {
-        res.send({
-          message: `Cannot delete bill with id=${id}. Bill not found!`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: `Could not delete bill with id=${id}`
-      });
+  const num = await Bill.destroy({
+    where: { id: id },
+  });
+
+  if (num === 1) {
+    res.send({
+      message: 'Bill was deleted successfully!',
     });
-};
+  } else {
+    res.send({
+      message: (`Cannot delete bill with id=${id}. Bill not found!`),
+    });
+  }
+});
