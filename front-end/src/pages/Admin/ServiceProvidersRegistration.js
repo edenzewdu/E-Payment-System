d873f162c2dfe1f68d48553db2e9193d9e7ee16d
eden zewdu <edenzewdu434@gmail.com>
2023-10-15 06:25:58 +0300

    updating service providers file chage

--- front-end/src/pages/Admin/ServiceProvidersRegistration.js
+++ front-end/src/pages/Admin/ServiceProvidersRegistration.js
@@ -1,9 +1,6 @@
-
-
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, URL } from 'react';
 import axios from 'axios';
-import { UserOutlined } from '@ant-design/icons';
+import { UploadOutlined } from '@ant-design/icons';
 import { Layout, Menu, Avatar, Button, message, Form, Input, Upload, Modal, Spin } from 'antd';
 import Dashboard from './Dashboard';
 import { useNavigate } from 'react-router-dom';
@@ -49,54 +46,35 @@ const ServiceProviderRegistrationForm = () => {
     );
   }
 
-  const validateForm = () => {
-    const newErrors = {};
-
-    if (!formData.serviceProviderBIN) {
-      newErrors.serviceProviderBIN = 'Business Identification Number is required';
+  const validateForm = async () => {
+    try {
+      await form.validateFields();
+      return isFileValid(file);
+    } catch (error) {
+      const newErrors = {};
+      error.errorFields.forEach((field) => {
+        newErrors[field.name[0]] = field.errors[0];
+      });
+      setErrors(newErrors);
+      return false;
     }
-
-    if (!formData.serviceProviderName) {
-      newErrors.serviceProviderName = 'ServiceProviderName is required';
-    }
-
-    if (!formData.servicesOffered) {
-      newErrors.servicesOffered = 'Services Offered is required';
-    }
-
-    if (!formData.BankName) {
-      newErrors.BankName = 'Bank Name is required';
-    }
-
-    if (!formData.BankAccountNumber) {
-      newErrors.BankAccountNumber = 'Bank Account Number is required';
-    }
-
-    if (!formData.phoneNumber) {
-      newErrors.phoneNumber = 'Phone Number is required';
-    } else if (!/^\+?\d+$/.test(formData.phoneNumber)) {
-      newErrors.phoneNumber = 'Phone Number is invalid';
-    }
-
-    if (!file) {
-      newErrors.serviceProviderAuthorizationLetter = "Service Authorization Letter is required";
-    }
-    else if (!isFileValid(formData.serviceProviderAuthorizationLetter)) {
-      newErrors.serviceProviderAuthorizationLetter = "Invalid file format. Only JPG, JPEG, or PNG files are allowed.";
-    }
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
   };
 
   function isFileValid(file) {
+    if (!file) {
+      message.error('No file selected.');
+      return false;
+    }
+
     const allowedTypes = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
     const fileExtension = file.name.split('.').pop().toLowerCase();
+
     if (!allowedTypes.includes(`image/${fileExtension}`)) {
       message.error('Invalid file type. Please select an image file (JPEG, JPG, PNG, GIF).');
       return false;
+    } else {
+      return true;
     }
-    else return true;
   }
 
   const handleChange = (e) => {
@@ -107,16 +85,15 @@ const ServiceProviderRegistrationForm = () => {
     }));
   };
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
+  const handleFileChange = (info) => {
+    const file = info.file.originFileObj;
     setFile(file);
     const url = URL.createObjectURL(file);
     setServiceProviderAuthorizationLetterUrl(url);
-    setFile(file);
   };
 
   const handleSubmit = async () => {
-    if (validateForm()) {
+    if (await validateForm()) {
       try {
         const formDataToSend = new FormData();
         formDataToSend.append('serviceProviderBIN', formData.serviceProviderBIN);
@@ -151,21 +128,17 @@ const ServiceProviderRegistrationForm = () => {
         message.success('Service provider registered successfully!');
 
       } catch (error) {
-        message.error('Error submitting form:')
-        console.error('Error submitting form:', error);
-        // Handle the error, show an error message, etc.
+        message.error('Failed to register service provider. Please try again.');
+        console.error('Error:', error);
       }
     }
   };
-
   return (
     <Dashboard
       content={
-        <Form name="serviceProviderRegistrationForm"
-          layout="vertical"
-          onFinish={handleSubmit}>
+        <Form name="serviceProviderRegistrationForm" layout="vertical" onFinish={handleSubmit}>
           <h1>Service provider Registration</h1>
-
+  
           <Form.Item
             label="Business Identification Number"
             name="serviceProviderBIN"
@@ -175,7 +148,7 @@ const ServiceProviderRegistrationForm = () => {
           >
             <Input name="serviceProviderBIN" onChange={handleChange} placeholder="Enter BIN" />
           </Form.Item>
-
+  
           <Form.Item
             label="Service Provider Name"
             name="serviceProviderName"
@@ -185,7 +158,7 @@ const ServiceProviderRegistrationForm = () => {
           >
             <Input name="serviceProviderName" onChange={handleChange} placeholder="Enter Name" />
           </Form.Item>
-
+  
           <Form.Item
             label="Services Offered"
             name="servicesOffered"
@@ -195,7 +168,7 @@ const ServiceProviderRegistrationForm = () => {
           >
             <Input name="servicesOffered" onChange={handleChange} placeholder="Enter Services Offered" />
           </Form.Item>
-
+  
           <Form.Item
             label="Bank Name"
             name="BankName"
@@ -205,7 +178,7 @@ const ServiceProviderRegistrationForm = () => {
           >
             <Input name="BankName" onChange={handleChange} placeholder="Enter Bank Name" />
           </Form.Item>
-
+  
           <Form.Item
             label="Bank Account Number"
             name="BankAccountNumber"
@@ -215,7 +188,7 @@ const ServiceProviderRegistrationForm = () => {
           >
             <Input name="BankAccountNumber" onChange={handleChange} placeholder="Enter Bank Account Number" />
           </Form.Item>
-
+  
           <Form.Item
             label="Phone Number"
             name="phoneNumber"
@@ -225,30 +198,30 @@ const ServiceProviderRegistrationForm = () => {
           >
             <Input name="phoneNumber" onChange={handleChange} placeholder="Enter Phone Number" />
           </Form.Item>
-
+  
           <Form.Item
-            label="serviceProviderAuthorizationLetter"
+            label="Service Provider Authorization Letter"
             name="serviceProviderAuthorizationLetter"
             validateStatus={errors.serviceProviderAuthorizationLetter && 'error'}
             help={errors.serviceProviderAuthorizationLetter}
             rules={[{ required: true }]}
           >
-            <Input
-              type="file"
+            <Upload
               name="serviceProviderAuthorizationLetter"
-              id="serviceProviderAuthorizationLetter"
-              accept=".jpeg, .jpg, .png, .gif"
-              validateStatus={errors.serviceProviderAuthorizationLetter && 'error'}
+              accept=".jpeg,.jpg,.png,.gif"
               onChange={handleFileChange}
-              help={errors.serviceProviderAuthorizationLetter}
-              rules={[{ required: true }]}
-              style={{ width: 'fit-content' }}
-            />
+              beforeUpload={(file) => {
+                setFile(file);
+                return false;
+              }}
+            >
+              <Button icon={<UploadOutlined />}>Click to Upload</Button>
+            </Upload>
             {serviceProviderAuthorizationLetterUrl && (
               <img src={serviceProviderAuthorizationLetterUrl} alt="Auth Letter" style={{ width: '200px' }} />
             )}
           </Form.Item>
-
+  
           <Form.Item>
             <Button type="primary" htmlType="submit">
               Register
@@ -258,6 +231,6 @@ const ServiceProviderRegistrationForm = () => {
       }
     />
   );
-};
+    };
 
 export default ServiceProviderRegistrationForm;
